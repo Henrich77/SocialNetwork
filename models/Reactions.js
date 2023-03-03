f27efb51b384bd16e7987a0f355bd5b4f138c580
Henrich Tanis <henrich@Henrichs-MacBook-Pro.local>
2023-03-03 14:59:16 -0600

    modified the reactions schema

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,22 +1,19 @@
 const { Schema, Types } = require('mongoose');
 
-const assignmentSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    assignmentId: {
+    reactionId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    assignmentName: {
+    reactionBody: {
       type: String,
       required: true,
       maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
     },
-    score: {
-      type: Number,
+    username: {
+      type: String,
       required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
     },
     createdAt: {
       type: Date,
@@ -31,4 +28,4 @@ const assignmentSchema = new Schema(
   }
 );
 
-module.exports = assignmentSchema;
+module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -30,11 +30,17 @@ const userSchema = new Schema(
   },
   {
     toJSON: {
+      virtuals:true,
       getters: true,
     },
   }
 );
 
-const Student = model('user', userSchema);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
 
 module.exports = Student;
